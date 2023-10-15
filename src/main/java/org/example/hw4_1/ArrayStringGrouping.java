40650be1d4902e13a5b040b05353cf5e78d02c17
Igor Simakov <i.simakov@banki.ru>
2023-10-15 19:27:36 +0800

    fix maven config

--- src/main/java/org/example/hw4_1/ArrayStringGrouping.java
+++ src/main/java/org/example/hw4_1/ArrayStringGrouping.java
@@ -1,27 +1,36 @@
 package org.example.hw4_1;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class ArrayStringGrouping {
     public static void main(final String[] args) {
-        final String[] arrayString = new String[]{"red", "maroon", "red", "yellow", "maroon", "green", "maroon", "green", "maroon", "green", "maroon", "green", "maroon", "green", "maroon", "green"};
+        final String[] arrayString = new String[]{"red", "maroon", "red", "yellow", "maroon", "green", "maroon",
+                "green", "maroon", "green", "maroon", "green", "maroon", "green", "maroon", "green"};
         stringGrouping(arrayString);
     }
 
+    //FlyableRunable
+
+    //Bird impl FlyableRunable
+    //run -> throw new NotImpl();
     public static void stringGrouping(final String[] arr) {
         final Map<String, Integer> hmStringGroup = new HashMap<>();
         int countWord = 1;
-        for (String word: arr
-             ) {
+        for (String word : arr) {
+
+            // todo: getOrDefault()
+            //todo: putIfAbsent()
             if (hmStringGroup.containsKey(word)) {
                 countWord = hmStringGroup.get(word);
                 countWord++;
                 hmStringGroup.put(word, countWord);
-               } else {
+            } else {
                 hmStringGroup.put(word, 1);
-                }
-               }
+            }
+        }
         for (Map.Entry<String, Integer> entry : hmStringGroup.entrySet()) {
             System.out.println(entry.getKey() + " повторений = " + entry.getValue());
         }
---
+++ src/main/java/org/example/hw4_1/MyLinkedList.java
@@ -0,0 +1,39 @@
+package org.example.hw4_1;
+
+// todo: implement
+public class MyLinkedList<E> implements MyList<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+}
