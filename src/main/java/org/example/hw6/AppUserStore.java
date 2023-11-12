eb71367e15feb8946a24b3978aab9153ced7c15b
Инна Ларина <innalar@mail.ru>
2023-11-12 13:36:59 +0500

    add User to File

--- src/main/java/org/example/hw6/AppUserStore.java
+++ src/main/java/org/example/hw6/AppUserStore.java
@@ -1,5 +1,7 @@
 package org.example.hw6;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -15,10 +17,21 @@ public class AppUserStore {
                        //.filter(u -> u.getId() == 2)
                        .toList();
     }
+
     public static User getUserFromFile(final String fileName, final int pid) throws IOException {
         List<User> users = getUsersFromFile(fileName);
         return users.stream().filter(u -> u.getId() == pid).toList().get(0);
     }
+
+    public static void addUserToFile(final User user, final String fileName) throws IOException {
+            FileWriter writer = new FileWriter(fileName, true);
+            BufferedWriter bufferWriter = new BufferedWriter(writer);
+            String text = String.format("%s; %s; %s", user.getId(), user.getLastName(), user.getPhoneNumber());
+            bufferWriter.write( System.lineSeparator());
+            bufferWriter.write(text);
+            bufferWriter.close();
+       }
+
     public static void main(final String[] args) throws IOException {
        List<User> users = getUsersFromFile("user.csv");
        for (User user:users
@@ -27,5 +40,6 @@ public class AppUserStore {
        }
        User user = getUserFromFile("user.csv", 1);
         System.out.println(user.getLastName());
+        addUserToFile(User.builder().id(4).lastName("Dozorova").phoneNumber("2272519").build(), "user.csv");
    }
 }
